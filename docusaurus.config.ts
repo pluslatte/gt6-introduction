bcd306cb73a7deacc567aa2a1d4e22d2b0bab496
pluslatte <debrisfield0201@gmail.com>
2024-10-22 16:04:50 +0900

    update

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -3,8 +3,8 @@ import type { Config } from '@docusaurus/types';
 import type * as Preset from '@docusaurus/preset-classic';
 
 const config: Config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
+  title: 'gregtech 6 ガイド',
+  tagline: 'work in progress',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
